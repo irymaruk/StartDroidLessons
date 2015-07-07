0a238645068bca664c1f39bff4682696bdfff684
igorja <rymaruk@gmail.com>
2015-07-07 13:57:27 +0300

    lesson 20 is done

--- p0161_dynamiclayout/src/main/java/com/igorja/p0161_dynamiclayout/MainActivity.java
+++ p0161_dynamiclayout/src/main/java/com/igorja/p0161_dynamiclayout/MainActivity.java
@@ -1,38 +1,59 @@
 package com.igorja.p0161_dynamiclayout;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.Gravity;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+;
+;
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+        // создание LinearLayout
+        LinearLayout linLayout = new LinearLayout(this);
+        // установим вертикальную ориентацию
+        linLayout.setOrientation(LinearLayout.VERTICAL);
+        // создаем LayoutParams  
+        LayoutParams linLayoutParam = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        // устанавливаем linLayout как корневой элемент экрана 
+        setContentView(linLayout, linLayoutParam);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        LayoutParams lpView = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        TextView tv = new TextView(this);
+        tv.setText("TextView");
+        tv.setLayoutParams(lpView);
+        linLayout.addView(tv);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        Button btn = new Button(this);
+        btn.setText("Button");
+        linLayout.addView(btn, lpView);
 
-        return super.onOptionsItemSelected(item);
+
+        LinearLayout.LayoutParams leftMarginParams = new LinearLayout.LayoutParams(
+                LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        leftMarginParams.leftMargin = 50;
+
+        Button btn1 = new Button(this);
+        btn1.setText("Button1");
+        linLayout.addView(btn1, leftMarginParams);
+
+
+        LinearLayout.LayoutParams rightGravityParams = new LinearLayout.LayoutParams(
+                LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        rightGravityParams.gravity = Gravity.RIGHT;
+
+        Button btn2 = new Button(this);
+        btn2.setText("Button2");
+        linLayout.addView(btn2, rightGravityParams);
     }
 }
---
+++ p0181_dynamiclayout3/src/main/java/com/igorja/p0181_dynamiclayout3/MainActivity.java
@@ -0,0 +1,59 @@
+package com.igorja.p0181_dynamiclayout3;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+
+public class MainActivity extends Activity implements SeekBar.OnSeekBarChangeListener {
+
+    SeekBar sbWeight;
+    Button btn1;
+    Button btn2;
+
+    LinearLayout.LayoutParams lParams1;
+    LinearLayout.LayoutParams lParams2;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        sbWeight = (SeekBar) findViewById(R.id.sbWeight);
+        sbWeight.setOnSeekBarChangeListener(this);
+
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+
+        lParams1 = (LinearLayout.LayoutParams) btn1.getLayoutParams();
+        lParams2 = (LinearLayout.LayoutParams) btn2.getLayoutParams();
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress,
+                                  boolean fromUser) {
+        int leftValue = progress;
+        int rightValue = seekBar.getMax() - progress;
+        // настраиваем вес
+        lParams1.weight = leftValue;
+        lParams2.weight = rightValue;
+        // btn1.setText автоматически вызывает и requestLayout()
+        //btn1.requestLayout();
+        //btn2.requestLayout();
+        // в текст кнопок пишем значения переменных
+        btn1.setText(String.valueOf(leftValue));
+        btn2.setText(String.valueOf(rightValue));
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+    }
+}
