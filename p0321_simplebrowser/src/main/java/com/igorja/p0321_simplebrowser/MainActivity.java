9d5f4ce7a8c2aa84958ca151f503532eb8558522
igorja <rymaruk@gmail.com>
2015-07-08 19:35:53 +0300

    lesson 33 is done

---
+++ p0321_simplebrowser/src/main/java/com/igorja/p0321_simplebrowser/MainActivity.java
@@ -0,0 +1,25 @@
+package com.igorja.p0321_simplebrowser;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        (findViewById(R.id.btnWeb)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.ya.ru")));
+            }
+        });
+    }
+}
